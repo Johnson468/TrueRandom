9f8cb9431d6ccafb2fa6b557bd87fb17354905c7
Johnson468 <jjohn468@uncc.edu>
2018-01-21 13:47:50 -0500

    Initial commit

---
+++ src/mainPackage/TRand.java
@@ -0,0 +1,40 @@
+package mainPackage;
+
+import java.io.File;
+import java.security.SecureRandom;
+
+public class TRand {
+	
+	File fileDirectory;
+	File currentFile;
+	String s = "";
+	public TRand(String filePath) {
+		filePath = filePath.replace("\\", "/");
+		if(!new File(filePath).isDirectory()) {
+			return;
+		}
+		fileDirectory = new File(filePath);
+	}
+	public TRand() {
+		
+	}
+	
+	
+	private String loopFiles(File f) {
+		if(f!=null && f.isDirectory()) {
+			for(File file: f.listFiles()) {
+				loopFiles(file);
+			}
+		} else if (f!=null && f.isFile()) {
+			s+=String.valueOf(f.length());
+		} else {
+			return null;
+		}
+		System.out.println(s);
+		return s;
+	}
+	public String loopFiles() {
+		return loopFiles(fileDirectory);
+	}
+	
+}
---
+++ src/manualTestFiles/main.java
@@ -0,0 +1,11 @@
+package manualTestFiles;
+import mainPackage.TRand;
+public class main {
+
+	public static void main(String[] args) {
+		TRand trand = new TRand("C:\\Checksum");
+		System.out.println(trand.loopFiles());
+		
+	}
+
+}
